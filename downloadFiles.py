c12175a950f209f8004cbe6d6e23218f4065982f
mechanico <32109749+mechanico@users.noreply.github.com>
2018-01-09 21:40:53 +0100

    Update downloadFiles.py

--- downloadFiles.py
+++ downloadFiles.py
@@ -3,18 +3,43 @@ from xml.etree import ElementTree
 import wget
 import sys
 import socket
+import os
+
+def downloadWorker(tree, twonkyServer, downloadPath):
+	for child in tree.iter("res"):
+		if "SM" in child.text or "AVI" in child.text or "MOV" in child.text:
+			url = child.text.replace("127.0.0.1",twonkyServer)
+			splited = url.split("/")
+			splitedTwo = splited[len(splited)-1]
+			fileUrl = downloadPath + "/" + splitedTwo[:splitedTwo.find("?")]
+			if not os.path.isfile(fileUrl):
+				try: 
+					wget.download(url, downloadPath)
+				except socket.error, e:
+					if 'Errno 111' in e:
+						print '*** Connection refused ***'
+						print '*** Url: %s ***' %url
+						pass
+					elif 'Errno 10054' in e:
+						print '*** Connection closed by Remote Host ***'
+						print '*** Url: %s ***' %url
+						pass
+			else:
+				pass
+			
+	return
 
 def downloadFiles(twonky, twonkyServer, downloadPath):
 	twonkyFolderStart = twonky.find("#")
 	if twonkyFolderStart != -1:
-		twonkyFinalUrl = createTwonkyUrl(twonky, twonkyFolderStart)
+		twonkyFinalUrl = createTwonkyUrl(twonky, twonkyServer, twonkyFolderStart)
 		response = requests.get(twonkyFinalUrl)
 		tree = ElementTree.fromstring(response.content)
 		for child in tree.iter("childCount"):
 			pictureCount = child.text
 			break
 		if int(pictureCount) >= 30:
-			getAll = raw_input("Found %s pictures, get all? [Y,N]"%pictureCount)
+			getAll = raw_input("Found %s pictures, get all? [Y, N] "%pictureCount)
 		else:
 			getAll = "N"
 		if getAll.upper() == "Y":
@@ -25,23 +50,18 @@ def downloadFiles(twonky, twonkyServer, downloadPath):
 				downloadUrl = twonkyFinalUrl.replace("start=0", "start=" + str(pics))
 				response = requests.get(downloadUrl)
 				tree = ElementTree.fromstring(response.content)
-				for child in tree.iter("res"):
-					if "SM" in child.text or "AVI" in child.text or "MOV" in child.text:
-						url = child.text.replace("127.0.0.1",twonkyServer)
-						wget.download(url, downloadPath)
+				downloadWorker(tree, twonkyServer, downloadPath)
 		elif getAll.upper() == "N":
-			for child in tree.iter("res"):
-				if "SM" in child.text or "AVI" in child.text or "MOV" in child.text:
-					url = child.text.replace("127.0.0.1",twonkyServer)
-					wget.download(url, downloadPath)
+			downloadWorker(tree, twonkyServer, downloadPath)
 		else:
-			print "Invalid Input!"
+			print "*** Invalid Input! ***"
+		
 	else:
-		print "Invalid Url!"
+		print "*** Invalid Url! ***"
 		sys.exit()
 
 
-def createTwonkyUrl(twonky, twonkyFolderStart):
+def createTwonkyUrl(twonky, twonkyServer, twonkyFolderStart):
 	twonkyUrl = twonky[twonkyFolderStart+1:]
 	twonkyUrlFindOne = twonkyUrl.find(":9000")
 	twonkyUrlFindTwo = twonkyUrl.find(":9001")
@@ -54,28 +74,51 @@ def createTwonkyUrl(twonky, twonkyFolderStart):
 		twonkyFinalUrl = "http://" + twonkyServer + twonkyFinalUrlEnd
 		return twonkyFinalUrl
 	else:
-		print "Invalid Url!"
+		print "*** Invalid Url! ***"
 		sys.exit()
 
-if len(sys.argv) != 2 :
-		print "Usage: " + sys.argv[0] + " 'Downloadpath'"
+def checkCreateDir(downloadPath, twonkyServer):
+	directory = downloadPath + twonkyServer
+	if not os.path.exists(directory):
+		try:
+			os.makedirs(directory)
+			print "*** Directory %s created. ***" %directory
+		except OSError as e:
+			print "Error: " %e
+			sys.exit()
+	else:
+		print "*** Directory %s found. ***" %directory
+	return directory
+
+def main():
+	if len(sys.argv) != 2 :
+		print "Usage: " + sys.argv[0] + " [Downloadpath]'"
 		sys.exit()
-else:
-	downloadPath = sys.argv[1]
+	else:
+		downloadPath = sys.argv[1]
+	print "-" * 30
+	print "#SharingIsCaring"                                                 
+	print "-" * 30
 
-twonky = raw_input("Enter Twonky Folder Url: ")
+	twonky = raw_input("Enter Twonky Folder Url: ")
+	twonkyServerStart = twonky.find("http://")
+	twonkyServerEndOne = twonky.find(":9000")
+	twonkyServerEndTwo = twonky.find(":9001")
+	if twonkyServerEndOne != -1:
+		twonkyServer = twonky[twonkyServerStart+7:twonkyServerEndOne]
 
-twonkyServerStart = twonky.find("http://")
-twonkyServerEndOne = twonky.find(":9000")
-twonkyServerEndTwo = twonky.find(":9001")
-if twonkyServerEndOne != -1:
-	twonkyServer = twonky[twonkyServerStart+7:twonkyServerEndOne]
+	elif twonkyServerEndTwo != -1:
+		twonkyServer = twonky[twonkyServerStart+7:twonkyServerEndTwo]
+	else:
+		print "*** Invalid Url! ***"
+		sys.exit()
 
-elif twonkyServerEndTwo != -1:
-	twonkyServer = twonky[twonkyServerStart+7:twonkyServerEndTwo]
+	downloadFiles(twonky, twonkyServer, checkCreateDir(downloadPath, twonkyServer))
+	continueDownload = raw_input("\nContinue downloading? [Y, N] ")
+	if continueDownload.upper() == "Y":
+		main()
+	else:
+		print "\n*** Thanks for Sharing :) ***"
+		sys.exit()
 
-else:
-	print "Invalid Url!"
-	sys.exit()
-
-downloadFiles(twonky, twonkyServer, downloadPath)
+main()
